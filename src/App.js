3f4b39d907b72c950fd73a3b6a04b4f2a720fc39
shafiimam <shafi15-8072@diu.edu.bd>
2021-03-14 22:45:55 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import firebase from "firebase/app";
+import "./App.css";
+import "firebase/auth";
+import firebaseConfig from "./firebase.config";
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 function App() {
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    name: "",
+    email: "",
+    photo: "",
+  });
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const handleSignIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((res) => {
+        var { displayName, photoURL, email } = res.user;
+        const signedInUser = {
+          isSignedIn: true,
+          name: displayName,
+          email: email,
+          photo: photoURL,
+        };
+        setUser(signedInUser);
+      })
+      .catch((error) => {
+        // var errorCode = error.code;
+        // var errorMessage = error.message;
+        // var email = error.email;
+        // var credential = error.credential;
+        console.log(error);
+        console.log(error.message);
+      });
+  };
+  const handleSignOut = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then((res) => {
+        const signedOutUser = {
+          isSignedIn: false,
+          name: "",
+          photo: "",
+          email: "",
+        };
+        setUser(signedOutUser);
+      })
+      .catch((error) => {
+        console.log(error);
+        console.log(error.message);
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {user.isSignedIn ? (
+        <button onClick={handleSignOut}>Sing Out</button>
+      ) : (
+        <button onClick={handleSignIn}>sign in</button>
+      )}
+      {user.isSignedIn && (
+        <div>
+          <p> welcome, {user.name}</p>
+          <p>Your email:{user.email}</p>
+          <img src={user.photo} alt="" />
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDG0E9s8w2TwppZZQ-pt4z-NjQgj3zCIrU",
+    authDomain: "beyond-the-horizon-7178f.firebaseapp.com",
+    projectId: "beyond-the-horizon-7178f",
+    storageBucket: "beyond-the-horizon-7178f.appspot.com",
+    messagingSenderId: "785941679542",
+    appId: "1:785941679542:web:b9e067c76536f232274517"
+  };
+
+  export default firebaseConfig;
